3f27e3b5611d4f95a656abd8170c5a6c5f2ea53a
Moondust <58139687+moondust46@users.noreply.github.com>
2020-03-30 23:03:12 +0900

    음악 순위 크롤링_3주차 숙제_실행확인
    실행은 확인 되었으나, 왼쪽 공백 삭제 실패로 수정 중

--- music_crawling.py
+++ music_crawling.py
@@ -5,11 +5,11 @@ headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
-music_info = soup.select('#div.music-list-wrap > table.list-wrap > tbody > tr > td.info')
+music_info = soup.select('div.music-list-wrap > table.list-wrap > tbody > tr > td.info')
 rank = 1
 for music in music_info:
-    if a_tag is not None:
-        title = music.select_one('td.info >a.title.ellipsis')
-        singer = music.select_one('td.info > a.artist.ellipsis')
+        title = music.select_one('td.info >a.title.ellipsis').text
+        title.lstrip()
+        singer = music.select_one('td.info > a.artist.ellipsis').text
         print(rank, title, singer)
         rank += 1
